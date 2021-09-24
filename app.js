7baaf7635ae1856a1bbd505d5164cc539169b43e
PettLe <petewaterz@gmail.com>
2021-09-24 10:26:54 +0300

    simplest version of assignment ready. Still trying to add at least couple of quality of life elements. Not necessary now though

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 add = (a, b) => a + b;
 substract = (a, b) => a - b;
 multiply = (a, b) => a * b;
-divide = (a, b) => a / b;
+function divide(a, b) {
+    if (b === "0") {
+        return numStr = "Wohhoo, nope. Don't do this.";
+    }
+    return (a / b);
+}
 
 /* Function that takes an operator, and according to it calls a matching function from before. */
 
@@ -23,16 +28,27 @@ function operate(op, displayValues) {
 /* Using FOR loop for the buttons, we take their values and print it on to display.
 Simultaneously we add the values in to an array for later use. */
 
+
 const nums = document.getElementsByClassName("nums");
 let display = document.querySelector(".display");
-
+let numStr = "";
 let displayValues = [];
+
+
 
 for (i = 0; i < nums.length; i++) {
     nums[i].addEventListener("click", function() {
+        if (numStr.length < 10) {
+        numStr += this.value;
         display.textContent += this.value;
-  
-        displayValues.push(this.value);
+    } else {
+        const max = document.createElement("p");
+        display.appendChild(max);
+        max.setAttribute("style", "text-align: right"); 
+        if (max.textContent === "") {
+        max.textContent = "(MAX)";}
+    }
+        console.log(numStr.length);
         console.log(displayValues);
     });
     }
@@ -48,25 +64,33 @@ const operator = document.getElementsByClassName("operator");
 for (i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", function() {
         op = this.value;
+        if (numStr !== "") {
+        displayValues.push(numStr);}
+        if (displayValues.length > 1) {
+            calcResult();
+        }
         display.textContent += this.value;
-
+        numStr = "";
     })
 }
 
 const result = document.querySelector(".result");
-result.addEventListener("click", function() {
-    displayValues.push(operate(op, displayValues));
+result.addEventListener("click", calcResult)
+
+function calcResult() {
+    displayValues.push(numStr);
+    numStr = "";
     display.textContent = operate(op, displayValues);
     displayValues = [display.textContent];
     console.log(displayValues);
+    console.log(numStr);
  
-})
-
+}
 
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", function() {
     display.textContent = "";
+    numStr = "";
     displayValues = [];
     console.log(displayValues);
 });
-
