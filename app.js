9caf8582bcde39dac2789aa07292e17678bf3fec
PettLe <petewaterz@gmail.com>
2021-09-24 09:04:06 +0300

    fixed kinda big bug. Still need to make it work with multiple inputs

--- app.js
+++ app.js
@@ -16,24 +16,23 @@ function operate(op, displayValues) {
         return multiply(displayValues[0], displayValues[1]);
     } else if (op === "/") {
         return divide(displayValues[0], displayValues[1]);
-    } else {
-        console.log("Operator ei found")
-        console.log("operator is, btw, " + op);
-    }
+    } 
 }
 
 
 /* Using FOR loop for the buttons, we take their values and print it on to display.
 Simultaneously we add the values in to an array for later use. */
 
-const button = document.getElementsByClassName("button");
+const nums = document.getElementsByClassName("nums");
 let display = document.querySelector(".display");
-let numStr = "";
 
-for (i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", function() {
+let displayValues = [];
+
+for (i = 0; i < nums.length; i++) {
+    nums[i].addEventListener("click", function() {
         display.textContent += this.value;
-        numStr += this.value;
+  
+        displayValues.push(this.value);
         console.log(displayValues);
     });
     }
@@ -42,21 +41,21 @@ for (i = 0; i < button.length; i++) {
 /* Actual functionality comes from here. Making the buttons commit the actual function of
 showing the result on screen. Also saving everything in array.*/
 
-let displayValues = [];
+
 let op = "";
 const operator = document.getElementsByClassName("operator");
 
 for (i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", function() {
         op = this.value;
-        displayValues.push(numStr.slice(0, -1));
-        numStr = "";
+        display.textContent += this.value;
+
     })
 }
 
 const result = document.querySelector(".result");
 result.addEventListener("click", function() {
-    displayValues.push(numStr.slice(0, -1));
+    displayValues.push(operate(op, displayValues));
     display.textContent = operate(op, displayValues);
     displayValues = [display.textContent];
     console.log(displayValues);
@@ -67,7 +66,6 @@ result.addEventListener("click", function() {
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", function() {
     display.textContent = "";
-    numStr = "";
     displayValues = [];
     console.log(displayValues);
 });
